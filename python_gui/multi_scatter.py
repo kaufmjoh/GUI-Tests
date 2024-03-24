fa8b193916ab406cd5ebb64a9a775b2244c78885
John Kaufman <kaufmjoh@oregonstate.edu>
2024-03-24 00:28:27 -0700

    Make the class more generic

--- python_gui/multi_scatter.py
+++ python_gui/multi_scatter.py
@@ -6,13 +6,24 @@ from PyQt5.QtCore import Qt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
 class ScatterPlotWindow(QMainWindow):
-    def __init__(self, dataframe1, dataframe2, dataframe3):
+    #def __init__(self, dataframe1, dataframe2, dataframe3): #Make this part generic, take a single dataframe, where 1 column is the scattegory
+    def __init__(self, dataframe): #Make this part generic, take a single dataframe, where 1 column is the scattegory
         super().__init__()
         self.setWindowTitle('Scatter Plot')
         self.setGeometry(100, 100, 800, 600)
         
-        self.dataframes = [dataframe1, dataframe2, dataframe3]
-        
+
+
+        grouped = dataframe.groupby('c') 
+
+        self.dataframes = {}
+
+        for name, group in grouped:
+            self.dataframes[name] = group
+
+
+
+
         self.setup_ui()
         
     def setup_ui(self):
@@ -31,11 +42,12 @@ class ScatterPlotWindow(QMainWindow):
         self.slider.setMaximum(3)
         self.slider.setTickInterval(1)
         self.slider.setTickPosition(QSlider.TicksBelow)
-        self.slider.valueChanged.connect(self.update_plot)
+        self.slider.valueChanged.connect(self.update_plot) #Call this function when the slider changes
         layout.addWidget(self.slider)
         
         self.plot_scatter(1)
         
+    #Plot the scatter plot using matplotlib
     def plot_scatter(self, index):
         ax = self.canvas.figure.add_subplot(111)
         df = self.dataframes[index - 1]
@@ -44,7 +56,8 @@ class ScatterPlotWindow(QMainWindow):
         ax.set_ylabel('Y')
         ax.set_title(f'Scatter Plot {index}')
         self.canvas.draw()
-        
+    
+    #Callback function
     def update_plot(self):
         index = self.slider.value()
         self.plot_scatter(index)
@@ -57,14 +70,15 @@ class PlotCanvas(FigureCanvas):
 
 if __name__ == '__main__':
     # Example DataFrames (replace these with your own dataframes)
-    data1 = {'x': [1, 2, 3, 4, 5], 'y': [1, 2, 3, 4, 5]}
-    data2 = {'x': [1, 2, 3, 4, 5], 'y': [5, 4, 3, 2, 1]}
-    data3 = {'x': [1, 2, 3, 4, 5], 'y': [1, 4, 3, 2, 5]}
-    df1 = pd.DataFrame(data1)
-    df2 = pd.DataFrame(data2)
-    df3 = pd.DataFrame(data3)
+    data = {
+    'c': [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2],
+    'x': [1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5],
+    'y': [1, 2, 3, 4, 5, 5, 4, 3, 2, 1, 3, 4, 5, 1, 2]
+    }
+
+    df = pd.DataFrame(data)
     
     app = QApplication(sys.argv)
-    window = ScatterPlotWindow(df1, df2, df3)
+    window = ScatterPlotWindow(df)
     window.show()
     sys.exit(app.exec_())
