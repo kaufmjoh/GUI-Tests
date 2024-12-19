2585a5806da1eb58cb851c60fcbc7b908f73c53f
John Kaufman <kaufmjoh@oregonstate.edu>
2024-12-18 23:06:40 -0800

    Changed functionality such that pressing the button generates plots. Has some bugs to work out

--- pyQt5_tests/multiScatter.py
+++ pyQt5_tests/multiScatter.py
@@ -1,7 +1,7 @@
 import sys
 import pandas as pd
 import matplotlib.pyplot as plt
-from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QSlider, QLabel
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QSlider, QLabel, QPushButton
 from PyQt5.QtCore import Qt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 import random
@@ -12,50 +12,29 @@ HEIGHT = 7
 
 
 class ScatterPlotWindow(QMainWindow):
-    def __init__(self, dataframe): 
+    def __init__(self): 
         super().__init__()
         self.setWindowTitle('Scatter Plot')
         self.setGeometry(100, 100, 1600, 600)
         
-
-
-        grouped = dataframe.groupby('c') 
-
-        self.dataframes = {}
-
-        for name, group in grouped:
-            self.dataframes[name] = group
-
-        self.numPlots = len(self.dataframes)
-
-
         self.setup_ui()
         
     def setup_ui(self):
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
-        layout = QVBoxLayout(central_widget)
+        self.layout = QVBoxLayout(central_widget)
         
         self.canvas = PlotCanvas(self, width=WIDTH, height=HEIGHT)
-        layout.addWidget(self.canvas)
+        self.layout.addWidget(self.canvas)
         
-        self.slider_label = QLabel("Select Scatter Plot:")
-        layout.addWidget(self.slider_label)
-        
-        self.slider = QSlider(Qt.Horizontal)
-        self.slider.setMinimum(1)
-        self.slider.setMaximum(self.numPlots)
-        self.slider.setTickInterval(1)
-        self.slider.setTickPosition(QSlider.TicksBelow)
-        self.slider.valueChanged.connect(self.update_plot) #Call this function when the slider changes
-        
-        
-        # Set stretch factor for slider_label to make it smaller
-        layout.addWidget(self.slider, stretch=1)
-        layout.setStretchFactor(self.slider_label, 0)
+        #Setup a button that when pressed will generate the plots
+        self.button = QPushButton(self)
+        self.button.setFixedSize(150, 150)
+        self.buttonLabel = QLabel("Generate Plots", self.button)
+        self.button.clicked.connect(self.generatePlots)
+        self.layout.addWidget(self.button, stretch=1)
 
 
-        self.plot_scatter(1) #Default plot to graph
         
     #Plot the scatter plot using matplotlib
     def plot_scatter(self, index):
@@ -78,6 +57,55 @@ class ScatterPlotWindow(QMainWindow):
         index = self.slider.value()
         self.plot_scatter(index)
 
+    def generatePlots(self):
+        c = []
+        x = []
+        y = []
+
+        print("Generating Plots") #Why does this not print until after the window is closed?
+
+        #This many plots
+        for i in range(0, 90):
+
+            #This many x values
+            for j in range(0, 1000):
+                c.append(i)
+                x.append(j)
+                y.append(random.randint(1, 1000))
+
+
+        df = pd.DataFrame( {'c':c, 'x':x, 'y':y} )
+        
+        grouped = df.groupby('c') 
+
+        self.dataframes = {}
+
+        for name, group in grouped:
+            self.dataframes[name] = group
+
+        self.numPlots = len(self.dataframes)
+        
+
+        self.slider_label = QLabel("Select Scatter Plot:")
+        self.layout.addWidget(self.slider_label)
+        
+        self.slider = QSlider(Qt.Horizontal)
+        self.slider.setMinimum(1)
+        self.slider.setMaximum(self.numPlots)
+        self.slider.setTickInterval(1)
+        self.slider.setTickPosition(QSlider.TicksBelow)
+        self.slider.valueChanged.connect(self.update_plot) #Call this function when the slider changes
+        
+        
+        # Set stretch factor for slider_label to make it smaller
+        self.layout.addWidget(self.slider, stretch=1)
+        self.layout.setStretchFactor(self.slider_label, 0)
+        
+        self.plot_scatter(1) #Default plot to graph
+
+
+
+
 class PlotCanvas(FigureCanvas):
     def __init__(self, parent=None, width=WIDTH, height=HEIGHT, dpi=100):
         self.figure = plt.figure(figsize=(width, height), dpi=dpi)
@@ -85,29 +113,12 @@ class PlotCanvas(FigureCanvas):
         self.setParent(parent)
 
 
-def generatePlots():
-    c = []
-    x = []
-    y = []
 
-    #This many plots
-    for i in range(0, 90):
-
-        #This many x values
-        for j in range(0, 1000):
-            c.append(i)
-            x.append(j)
-            y.append(random.randint(1, 1000))
-
-
-    df = pd.DataFrame( {'c':c, 'x':x, 'y':y} )
-    
-    return df
 
 if __name__ == '__main__':
 
-    df = generatePlots()
+    #df = generatePlots()
     app = QApplication(sys.argv)
-    window = ScatterPlotWindow(df)
+    window = ScatterPlotWindow()
     window.show()
     sys.exit(app.exec_())
