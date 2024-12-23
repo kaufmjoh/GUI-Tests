94bd92160907348390405962861688d22f6a3332
John Kaufman <kaufmjoh@oregonstate.edu>
2024-12-23 11:09:31 -0800

    Changed the combineCsv process to make it duplicate the csv 25,000 times. Additionally, it will now output a column indicating what index of duplication it's on. This can definitely be cleaned up.

--- pyQt5_tests/combineCsvs/combineCsvs.c
+++ pyQt5_tests/combineCsvs/combineCsvs.c
@@ -5,7 +5,7 @@
 #define LINE_LENGTH 255
 
 //Combine File1 and File2 into outputFile, one line at a time
-void combineCsvs(char *inputFile1, char *inputFile2, char *outputFile)
+void combineCsvs(char *inputFile1, char *inputFile2, char *outputFile, int index)
 {
 
 	FILE *filePointer1;
@@ -15,7 +15,7 @@ void combineCsvs(char *inputFile1, char *inputFile2, char *outputFile)
 	//Open the input files for reading, and the output file for writing
 	filePointer1 = fopen(inputFile1, "r");
 	filePointer2 = fopen(inputFile2, "r");
-	filePointer3 = fopen(outputFile, "w");
+	filePointer3 = fopen(outputFile, "a");
 
 	if (filePointer1 == NULL || filePointer2 == NULL)
 	{
@@ -30,21 +30,31 @@ void combineCsvs(char *inputFile1, char *inputFile2, char *outputFile)
 
 	char currLine[LINE_LENGTH];
 
+	char indexAsString[LINE_LENGTH];
+	sprintf(indexAsString, "%d", index);
+	strcat(indexAsString, ",");
+
 	//Read and write to files, while the input files still have lines left
 	do
 	{
 		//Read a line from input file 1
 		endOfFile1 = fgets(currLine, LINE_LENGTH, filePointer1);
 		if (endOfFile1 != NULL){
-			printf("Curr line from file 1: %s \n", currLine);
-			fprintf(filePointer3, currLine);
+			//printf("Curr line from file 1: %s \n", currLine);
+			strcat(indexAsString, currLine);
+			fprintf(filePointer3, indexAsString);
+			sprintf(indexAsString, "%d", index);
+			strcat(indexAsString, ",");
 		}
 
 		//Read a line from input file 2
 		endOfFile2 = fgets(currLine, LINE_LENGTH, filePointer2);
 		if (endOfFile2 != NULL){
-			printf("Curr line from file 2: %s \n", currLine);
-			fprintf(filePointer3, currLine);
+			//printf("Curr line from file 2: %s \n", currLine);
+			strcat(indexAsString, currLine);
+			fprintf(filePointer3, indexAsString);
+			sprintf(indexAsString, "%d", index);
+			strcat(indexAsString, ",");
 		}
 
     } while(endOfFile1 != NULL || endOfFile2 != NULL);
@@ -61,14 +71,37 @@ void combineCsvs(char *inputFile1, char *inputFile2, char *outputFile)
 //argv[3] = pathToInputFile3
 int main(int argc, char **argv)
 {
-	
+	/*
+	int b = 0;
+	int c = 0;
+	int loops = 99999;
+	for(int i = 0; i < loops; i++)
+	{
+		for(int j = 0; j < loops; j++)
+		{
+			b = 1-b;
+		}
+
+		c++;
+		if(c == 1000)
+		{
+			printf("C incremented");
+			c = 0;
+		}
+	}*/
+
+
 	if (argc != 4)
 	{
 		printf("You must provide three file paths to perform csv combination! (file1, file2, and destination)\n");
 		return 0;
 	}
 
-	combineCsvs(argv[1], argv[2], argv[3]);
+	for(int i = 0; i < 25000; i++)
+	{
+
+		combineCsvs(argv[1], argv[2], argv[3], i);
+	}
 
 	return 0;
 }
