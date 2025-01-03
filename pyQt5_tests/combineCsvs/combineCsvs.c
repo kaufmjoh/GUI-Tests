fec509ea96e167115dc88d35dafae9cb12c0795d
John Kaufman <55894658+kaufmjoh@users.noreply.github.com>
2025-01-03 14:04:26 -0800

    Combine csvs (#5)
    * First commit of combineCsvs, which is a new branch to test a gui which will combine two csv files together
    
    * Created a new gui that allows the user to select to csvs using a dialog box
    
    * Remove excess files
    
    * Storing changes
    
    * Created combineCsvs.bat, which will compile and run the C program when called
    
    * The python code will now run the bat script when the user hits a button
    
    * Modified it so it will run the bat file with the input file names the user selected
    
    * The user can now use a file dialog box to select the destination the csv should be saved to
    
    * Changed the combineCsv process to make it duplicate the csv 25,000 times. Additionally, it will now output a column indicating what index of duplication it's on. This can definitely be cleaned up.
    
    * The python gui will now track the status of the C executable, and display the progress in a text label

---
+++ pyQt5_tests/combineCsvs/combineCsvs.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define LINE_LENGTH 255
+
+//Combine File1 and File2 into outputFile, one line at a time
+void combineCsvs(char *inputFile1, char *inputFile2, char *outputFile, int index)
+{
+
+	FILE *filePointer1;
+	FILE *filePointer2;
+	FILE *filePointer3;
+
+	//Open the input files for reading, and the output file for writing
+	filePointer1 = fopen(inputFile1, "r");
+	filePointer2 = fopen(inputFile2, "r");
+	filePointer3 = fopen(outputFile, "a");
+
+	if (filePointer1 == NULL || filePointer2 == NULL)
+	{
+		printf("Error reading from files!\n");
+		return;
+	}
+
+	//These keep track of whether we've reached the end of the file
+	void* endOfFile1 = 0;
+	void* endOfFile2 = 0;
+
+
+	char currLine[LINE_LENGTH];
+
+	char indexAsString[LINE_LENGTH];
+	sprintf(indexAsString, "%d", index);
+	strcat(indexAsString, ",");
+
+	//Read and write to files, while the input files still have lines left
+	do
+	{
+		//Read a line from input file 1
+		endOfFile1 = fgets(currLine, LINE_LENGTH, filePointer1);
+		if (endOfFile1 != NULL){
+			//printf("Curr line from file 1: %s \n", currLine);
+			strcat(indexAsString, currLine);
+			fprintf(filePointer3, indexAsString);
+			sprintf(indexAsString, "%d", index);
+			strcat(indexAsString, ",");
+		}
+
+		//Read a line from input file 2
+		endOfFile2 = fgets(currLine, LINE_LENGTH, filePointer2);
+		if (endOfFile2 != NULL){
+			//printf("Curr line from file 2: %s \n", currLine);
+			strcat(indexAsString, currLine);
+			fprintf(filePointer3, indexAsString);
+			sprintf(indexAsString, "%d", index);
+			strcat(indexAsString, ",");
+		}
+
+    } while(endOfFile1 != NULL || endOfFile2 != NULL);
+
+	//Close the files
+    fclose(filePointer1);
+	fclose(filePointer2);
+	fclose(filePointer3);
+}
+
+//Command line arguments should be of form
+//argv[1] = pathToInputFile1
+//argv[2] = pathToInputFile2
+//argv[3] = pathToInputFile3
+int main(int argc, char **argv)
+{
+	/*
+	int b = 0;
+	int c = 0;
+	int loops = 99999;
+	for(int i = 0; i < loops; i++)
+	{
+		for(int j = 0; j < loops; j++)
+		{
+			b = 1-b;
+		}
+
+		c++;
+		if(c == 1000)
+		{
+			printf("C incremented");
+			c = 0;
+		}
+	}*/
+
+
+	if (argc != 4)
+	{
+		printf("You must provide three file paths to perform csv combination! (file1, file2, and destination)\n");
+		return 0;
+	}
+	printf("Beginning C operations");
+
+	for(int i = 0; i < 25000; i++)
+	{
+
+		combineCsvs(argv[1], argv[2], argv[3], i);
+	}
+
+	printf("Finished C operations");
+
+	return 0;
+}
+
---
+++ pyQt5_tests/combineCsvs/combineCsvs.py
@@ -0,0 +1,183 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QLabel, QPushButton, QLineEdit, QHBoxLayout, QFileDialog
+import os
+import pandas as pd
+import subprocess
+import time
+import threading
+
+
+class CombineCsvsWindow(QMainWindow):
+    def __init__(self): 
+        super().__init__()
+        self.setWindowTitle('Scatter Plot')
+        self.setGeometry(100, 100, 1600, 600)
+        
+        self.progress = -1
+
+        self.setup_ui()
+
+    #Call the setup functions to setup the various layouts of the gui   
+    def setup_ui(self):
+        #Set up the central widget and the ui layout
+        centralWidget = QWidget()
+        self.setCentralWidget(centralWidget)
+        self.centralLayout = QHBoxLayout(centralWidget)
+        
+        self.setupGetFile1Box()
+        self.setupGetFile2Box()
+        self.setupResultsBox()
+
+    #Setup the layout for selecting the first csv file
+    def setupGetFile1Box(self):
+        #Setup a button to get the first csv file
+        self.file1Layout = QVBoxLayout()
+
+        self.file1Name = QLabel("<First file name will appear here>")
+        self.file1Layout.addWidget(self.file1Name)
+
+        self.button = QPushButton(self)
+        self.button.setFixedSize(200, 50)
+        self.buttonLabel = QLabel("Select first file for combination", self.button)
+        self.button.clicked.connect(self.getFile1)
+        self.file1Layout.addWidget(self.button, stretch=1)
+
+        self.centralLayout.addLayout(self.file1Layout)
+
+    #Setup the layout for selecting the second csv file
+    def setupGetFile2Box(self):
+        #Setup a button to get the second csv file
+        self.file2Layout = QVBoxLayout()
+
+        self.file2Name = QLabel("<Second file name will appear here>")
+        self.file2Layout.addWidget(self.file2Name)
+
+        self.button = QPushButton(self)
+        self.button.setFixedSize(200, 50)
+        self.buttonLabel = QLabel("Select second file for combination", self.button)
+        self.button.clicked.connect(self.getFile2)
+        self.file2Layout.addWidget(self.button, stretch=1)
+
+        self.centralLayout.addLayout(self.file2Layout)
+
+    #Setup the layout for where the output / results information will be stored
+    def setupResultsBox(self):
+        self.resultsLayout = QVBoxLayout()
+
+        self.destinationFileName = QLabel("<Destination file name will appear here>")
+        self.resultsLayout.addWidget(self.destinationFileName)
+
+        #Text label showing highest index
+        self.progressDisplay = QLabel("<Progress report will appear here>")
+        self.resultsLayout.addWidget(self.progressDisplay)
+
+
+        #Button to allow the user to select the destination
+        self.button = QPushButton(self)
+        self.button.setFixedSize(200, 50)
+        self.buttonLabel = QLabel("Select destination", self.button)
+        self.button.clicked.connect(self.getDestinationFile)
+        self.resultsLayout.addWidget(self.button, stretch=1)
+
+        #When this button is pressed, it will run the combine csvs script
+        self.button = QPushButton(self)
+        self.button.setFixedSize(200, 50)
+        self.buttonLabel = QLabel("Combine files", self.button)
+        self.button.clicked.connect(self.runCombinationScript)
+        self.resultsLayout.addWidget(self.button, stretch=1)
+
+        self.centralLayout.addLayout(self.resultsLayout)
+
+    #Open the dialog to get the first csv file
+    def getFile1(self):
+
+        options = QFileDialog.Options()
+        file_name, _ = QFileDialog.getOpenFileName(self, "Select File", "", "CSV Files (*.csv)", options=options)
+        if file_name:
+            print("Selected file 1:", file_name)
+            self.file1Name.setText(file_name)
+
+    #Open the dialog to get the second csv file
+    def getFile2(self):
+    
+        options = QFileDialog.Options()
+        file_name, _ = QFileDialog.getOpenFileName(self, "Select File", "", "CSV Files (*.csv)", options=options)
+        if file_name:
+            print("Selected file 2:", file_name)
+            self.file2Name.setText(file_name)
+
+    #Open the dialog for the user to enter csv destination
+    def getDestinationFile(self):
+        print("SUS!")
+        dialog = QFileDialog(self)
+        dialog.setFileMode(QFileDialog.AnyFile)
+        dialog.setViewMode(QFileDialog.Detail)
+            
+        #dialog.setNameFilters(["Images (*.png *.xpm *.jpg)", "Text files (*.txt)", "All files (*)"])
+        dialog.setNameFilters(["CSV Files (*.csv)"])
+        if dialog.exec_():
+            fileName = dialog.selectedFiles()[0]
+            
+            if not fileName.endswith(".csv"): #Add .csv to user's file name
+                fileName += ".csv"
+            
+            self.destinationFileName.setText(fileName)
+            print("Selected fileName", fileName)
+        else:
+            print("Failure question mark!")
+
+    #Call the script to combine the csvs
+    def runCombinationScript(self):
+
+        batFileName = "combineCsvs.bat"
+        cFileName = "combineCsvs.c"
+        exeFileName = "combineCsvs.exe"
+        inputFile1 = self.file1Name.text()
+        inputFile2 = self.file2Name.text()
+        outputFile = self.destinationFileName.text()
+
+        cmd = batFileName + " " + cFileName + " " + exeFileName + " " + inputFile1 + " " + inputFile2 + " " + outputFile
+
+        #os.system(cmd)
+        #os.system("combineCsvs.bat combineCsvs.c combineCsvs.exe data/ints.csv data/chars.csv data/combo.csv")
+
+        subprocess.Popen([batFileName, cFileName, exeFileName, inputFile1, inputFile2, outputFile])
+
+        thread = threading.Thread(target=self.getProgressReport)
+        thread.daemon = True
+        thread.start()
+        #self.getProgressReport()
+
+
+    def getProgressReport(self):
+        temp_max_index = 0
+
+        time.sleep(2)
+
+        while(self.progress != temp_max_index):
+
+            try:
+                #Open csv, store in PD data frame
+                df = pd.read_csv(self.destinationFileName.text(), names = ['index', 'col1', 'col2', 'col3'])
+
+                #Get the maximum value from the first column, store in temp
+                self.progress = temp_max_index
+                temp_max_index = df['index'].max()
+                
+                self.progressDisplay.setText(str(temp_max_index))
+                
+
+            except: #Error was likely file wasn't created yet
+                temp_max_index = -2
+                print("You jumped the gun")
+                self.progressDisplay.setText("You jumped the gun")
+    
+
+
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    window = CombineCsvsWindow()
+    window.show()
+    sys.exit(app.exec_())
